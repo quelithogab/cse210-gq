1b126521ed9b9b5177e94a77f4415b7c5a0c59b7
quelithogab <quelithogabriel28@gmail.com>
2024-11-12 14:27:34 -0500

    Job resume added

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // The first job
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        // The second job
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
+        // Create a resume and add jobs to it
+        Resume resume = new Resume();
+        resume._name = "Quelitho Gabriel";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        // Display the resume
+        resume.Display();
     }
 }
