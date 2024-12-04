503a9722a2ac1f99632c51b985cdbb71538356a4
quelithogab <quelithogabriel28@gmail.com>
2024-12-04 15:23:45 -0500

    Project Week05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,13 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        BreathingActivity breathingActivity = new BreathingActivity(); 
-        breathingActivity.Run(); 
-        
-        ListingActivity listingActivity = new ListingActivity(); 
-        listingActivity.Run(); 
-        
-        ReflectingActivity reflectingActivity = new ReflectingActivity(); 
-        reflectingActivity.Run();
+
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
